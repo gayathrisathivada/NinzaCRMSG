681670bd4d4a77ad9275df481bbf20b63133984d
Hp <Hp@DESKTOP-NRBQO3B>
2025-12-18 07:44:52 +0530

    First time Committing the code.

---
+++ src/test/java/Demo/Practice1Test.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Practice1Test 
+{
+	@Test(groups = "Smoke")
+	public void CarTest()
+	{
+		System.out.println("Audi");
+	}
+
+}
---
+++ src/test/java/Demo/Practice2Test.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Practice2Test 
+{
+	@Test(groups = "Regression")
+	public void CarTest()
+	{
+		System.out.println("Yamaha Bike");
+	}
+
+}
