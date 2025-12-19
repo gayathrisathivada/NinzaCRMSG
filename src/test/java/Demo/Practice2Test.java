1479d82e188072de7a38e512405d073ecb377677
Hp <Hp@DESKTOP-NRBQO3B>
2025-12-19 07:27:21 +0530

    RE committed

--- src/test/java/Demo/Practice2Test.java
+++ src/test/java/Demo/Practice2Test.java
@@ -2,12 +2,15 @@ package Demo;
 
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class Practice2Test 
 {
 	@Test(groups = "Regression")
 	public void CarTest()
 	{
 		System.out.println("Yamaha Bike");
+		System.out.println("Royal Enfield");
 	}
 
 }
